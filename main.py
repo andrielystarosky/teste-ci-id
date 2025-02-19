cfc47cc2263479410fb4adfb87d4f9533ac0b2d9
Andriely <110883419+andrielystarosky@users.noreply.github.com>
2025-02-18 21:39:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-# main.py
-def soma(a, b):
-    return a + b
-
-def test_soma():
-    assert soma(2, 3) == 5
-    assert soma(-1, 1) == 0
-    print("Todos os testes passaram!")
-
-if __name__ == "__main__":
-    test_soma()
+print("Hello world")
